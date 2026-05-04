762c9f7eb47f02987d01cf6830f3a57f2fd08e3c
Artur de Maria <arturdemaria7@gmail.com>
2026-05-04 14:23:06 -0300

    feature/image-rasterize: minimap texture grid + color gradient

--- lib/Preenchimento.py
+++ lib/Preenchimento.py
@@ -177,7 +177,8 @@ def scanline_texture(superficie, pontos, uvs, textura, tex_w, tex_h):
 
                 if 0 <= tx < tex_w and 0 <= ty < tex_h:
                     cor = textura.get_at((tx, ty))
-                    setPixel(superficie, x, y, cor)
+                    if cor.a > 0:
+                        setPixel(superficie, x, y, cor)
 
 def one_scanline(superficie, x_inicial, x_final, y, cor):
     for xp in range(int(x_inicial), int(x_final)+1):
--- polygons/minimap.py
+++ polygons/minimap.py
@@ -2,9 +2,7 @@ from lib.Preenchimento import scanline_fill_gradiente, scanline_texture
 from lib.Primitivas import desenhar_poligono_recortado
 import pygame
 
-def draw_minimap(WIDTH, MINIMAPA_W, MINIMAPA_H, screen, padding):
-    minimap_bg = pygame.image.load("assets/minimap_bg.jpg")
-
+def draw_minimap(WIDTH, MINIMAPA_W, MINIMAPA_H, screen, padding, minimap_bg):
     x_min = WIDTH - MINIMAPA_W - padding
     y_min = padding
     x_max = WIDTH - padding
@@ -27,6 +25,7 @@ def draw_minimap(WIDTH, MINIMAPA_W, MINIMAPA_H, screen, padding):
     tex_w = minimap_bg.get_width()
     tex_h = minimap_bg.get_height()
 
+    scanline_fill_gradiente(screen, pontos_fundo_minimapa, [(70,70,70),(70,70,70),(0,143,127),(0,143,127)])
     scanline_texture(screen, pontos_fundo_minimapa, uvs_minimapa, minimap_bg, tex_w, tex_h)
     desenhar_poligono_recortado(screen, pontos_fundo_minimapa, (x_min, y_min, x_max, y_max), (150, 150, 150))
     
